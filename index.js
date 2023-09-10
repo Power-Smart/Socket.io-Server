ad87f563168aa2eb4e3d3dca27ff3bb9cac2e9b3
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-09-10 08:23:16 +0530

    basic chat config is done

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const {Server} = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+    }
+});
+
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected', socket.id);
+    });
+
+});
+
+
+server.listen(3010, () => {
+    console.log('listening on :3010');
+});
