af12090c4e730f17934bbc22777937415292f88c
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-09-13 14:23:16 +0530

    chat client diconnrt function done

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(cors());
 
 const server = http.createServer(app);
 
+let socketClientArr = {};
+
 const io = new Server(server, {
     cors: {
         origin: 'http://localhost:5173',
@@ -16,19 +18,42 @@ const io = new Server(server, {
 
 
 io.on('connection', (socket) => {
+
     console.log(socket.id);
 
     socket.on('send-message', (message) => {
         console.log(message);
-        socket.broadcast.emit('receive-message', message);
+
+        const parsedMsg = JSON.stringify(message);
+
+        if(parsedMsg.hasOwnProperty("flag") && parsedMsg.flag === "I"){
+
+            const sId = parsedMsg.senderId.toString();
+
+            socketClientArr.sId = socket.id;
+        }else if((parsedMsg.hasOwnProperty("flag") && parsedMsg.flag === "F")){
+            
+            const rId = parsedMsg.recieverId.toString();
+
+            io.to(rId).emit('recieve-message', parsedMsg.message);
+
+        }
+
     });
 
     socket.on('disconnect', () => {
+        if(Object.values(socketClientArr).includes(socket.id)){
+
+            Object.keys(socketClientArr).forEach((key, value) => {
+                if (socketClientArr[value] === socket.id) {
+                    delete socketClientArr[key];
+                }
+            });
+        }
         console.log('user disconnected', socket.id);
     });
 
 });
-
 
 server.listen(3010, () => {
     console.log('listening on :3010');
